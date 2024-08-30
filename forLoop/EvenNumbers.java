56966b0cd6c840f8b68aad1b5b7b517107a280d9
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-08-30 22:05:15 +0530

    Add files via upload

---
+++ forLoop/EvenNumbers.java
@@ -0,0 +1,12 @@
+class EvenNumbers
+{
+	public static void main(String[] args) {
+		for (int number =1;number<100 ;number++ ) {
+			
+			if(number%2==0)
+			{
+				System.out.print(number+" ");
+			}
+		}
+	}
+}
