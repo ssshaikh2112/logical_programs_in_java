7ce1c27844c592e63ea4b80481f61f92cd6b504d
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-09-02 12:33:00 +0530

    Add files via upload

---
+++ forLoop/NumberIntoWords.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class NumberIntoWords
+{
+	// WAJP to convert number into words	(123 ---> one two three)
+	public static void main(String[] args) 
+	{
+		// 123	--> onetwothree
+		Scanner scan=new Scanner(System.in);
+		int number=scan.nextInt();
+		//char number=scan.next().charAt(0);
+
+		for (char i='0';i<'122' ;i++ ) 
+		{
+			if(number==)
+			
+		}
+		
+	}
+}
