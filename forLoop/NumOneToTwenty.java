21174aaf4cd8ca23cf42d2df1098df522852cf96
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-09-03 12:12:39 +0530

    Add files via upload

---
+++ forLoop/NumOneToTwenty.java
@@ -0,0 +1,15 @@
+class NumOneToTwenty
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Print Number from 1 - 20 ");
+		System.out.println();
+
+		for (int number=1;number<=20 ;number++ )
+		{
+			System.out.print(number+" ");
+
+			
+		}
+	}
+}
