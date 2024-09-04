d93d59db34b14e508aec2bd246881f6f8781d4d1
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-09-04 13:00:36 +0530

    Add files via upload

---
+++ forLoop/OddNumbers.java
@@ -0,0 +1,12 @@
+class OddNumbers
+{
+	public static void main(String[] args) {
+		for (int number =100;number>=1 ;number-- ) {
+			
+			if(number%2!=0)
+			{
+				System.out.print(number+" ");
+			}
+		}
+	}
+}
