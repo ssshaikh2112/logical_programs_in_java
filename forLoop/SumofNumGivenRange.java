240d4412487def9e053c4ba0eaa6992129bfc263
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-09-05 18:05:18 +0530

    Add files via upload

---
+++ forLoop/SumofNumGivenRange.java
@@ -0,0 +1,15 @@
+class SumofNumGivenRange
+{
+	public static void main(String[] args) 
+	{
+		int sum=0;
+		for (int number=1;number<=20 ;number++ ) 
+		{
+			sum+=number;
+		//	System.out.print(sum+" ");
+			
+		}
+		System.out.print("Sum of 1 to 20 number is : "+sum+" ");
+		
+	}
+}
