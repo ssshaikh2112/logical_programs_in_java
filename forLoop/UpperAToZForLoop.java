8bb121f37af13dc36b365b642b658e78c288d338
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-09-05 18:05:39 +0530

    Add files via upload

---
+++ forLoop/UpperAToZForLoop.java
@@ -0,0 +1,22 @@
+class UpperAToZForLoop
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Print the A - Z");
+		System.out.println("============== First  Logic ==============");
+
+		for (char alpha='A';alpha<='Z' ;alpha++ ) 
+		{
+			System.out.print(alpha+" ");
+		}
+		System.out.println();
+		System.out.println("============== Second Logic ==============");
+		for (char alpha=65;alpha<=90 ;alpha++ ) 
+		{
+			System.out.print(alpha+" ");
+		}
+
+		
+		
+	}
+}
