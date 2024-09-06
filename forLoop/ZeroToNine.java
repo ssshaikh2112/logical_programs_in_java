0accc0f14ebf7508b8d36e9830a9cba14b0538a3
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-09-06 21:29:18 +0530

    Add files via upload

---
+++ forLoop/ZeroToNine.java
@@ -0,0 +1,14 @@
+class ZeroToNine
+{
+	public static void main(String[] args) 
+	{
+		for (int number=0;number<=9 ;number++) 
+		{
+			// 0 1 2 3 4 5 6 7 8 9
+			System.out.print(number+" ");
+
+			
+		}
+		
+	}
+}
