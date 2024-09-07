5c1ff77474e49be6af8dd4582483b1ee93aa9d84
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-09-07 09:07:25 +0530

    Add files via upload

---
+++ Logical/RandomAlphabets.java
@@ -0,0 +1,12 @@
+class RandomAlphabets
+{
+	public static void main(String[] args) 
+	{
+		for (char i='A';i<='Z' ;i+=3 )	// random  alphabets got after 3 
+		{
+			System.out.print(i+" ");
+			
+		}
+		
+	}
+}
