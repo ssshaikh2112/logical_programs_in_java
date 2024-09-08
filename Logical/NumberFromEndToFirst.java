2c314b7950f56d235c195ba6118e6bb12887d022
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-09-08 13:58:10 +0530

    Add files via upload

---
+++ Logical/NumberFromEndToFirst.java
@@ -0,0 +1,13 @@
+class NumberFromEndToFirst
+{
+	public static void main(String[] args) 
+	{
+		
+		// Prints number in Backward order from 20 to 1
+
+		for (int number=20;number>=1 ;number-- ) 
+		{
+			System.out.print(number+" ");		
+		}		
+	}
+}
