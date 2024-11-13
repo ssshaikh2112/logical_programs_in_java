e9964cea497d6bce2319e2b5e441fd89f554e7f0
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-11-13 21:30:25 +0530

    Add files via upload

---
+++ Array/Array1.java
@@ -0,0 +1,13 @@
+class Array1
+{
+	public static void main(String[] args) 
+	{
+		int[] a=new int[2];
+		a[0]=10;
+		a[1]=20;
+		for (int i=0;i<a.length ;i++ ) 
+		{
+			System.out.println(a[i]);
+		}
+	}
+}
