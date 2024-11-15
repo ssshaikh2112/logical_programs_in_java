8b06b129f83526ba6c616dabc14a109b8491008d
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-11-15 21:50:44 +0530

    Add files via upload

---
+++ Operator/Program1MulOp.java
@@ -0,0 +1,13 @@
+class Program1MulOp
+{
+	public static void main(String[] args) 
+	{
+		double calculation;
+
+		calculation=((9.5*4.5*2.5*3)/(45.5*3.5));	
+		System.out.println(calculation);
+
+		//	OutPut
+		//  2.013343799058085
+	}
+}
