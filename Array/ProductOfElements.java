4dc956b219f0f37ab6847a4cd3f2225c5732378c
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2025-01-14 15:36:53 +0530

    Add files via upload

---
+++ Array/ProductOfElements.java
@@ -0,0 +1,13 @@
+class ProductOfElements
+{
+	public static void main(String[] args) 
+	{
+		int[] array ={1,2,3,4,5,6,7,8,9};
+		int prod=1;
+		for (int i=0;i<array.length ;i++ ) 
+		{
+			prod*=array[i];
+		}
+		System.out.println(prod);
+	}
+}
