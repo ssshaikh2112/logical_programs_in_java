486cb48fd62555089f42678f3cbe04825825acf2
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2025-02-07 22:55:34 +0530

    Add files via upload

---
+++ DSA/Arrays78CharDontChangePosiAfterReverse.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class Arrays78CharDontChangePosiAfterReverse 
+{
+
+	public static void main(String[] args) 
+	{
+		String str="alphxxdida";
+		
+		int count=0;
+		for (int i = 0 , j=str.length()-1; i < str.length(); i++ ,j--) {
+			if(str.charAt(i)==str.charAt(j))
+			{
+				count++;
+			}	
+		}
+		System.out.println(count);
+	}
+}
