763dc2d17d11c1f54c2f3f74888a655ffc0d09da
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2025-02-09 22:44:13 +0530

    Add files via upload

---
+++ DSA/Arrays79QScieLabExp.java
@@ -0,0 +1,18 @@
+package arrays;
+
+import java.util.Scanner;
+
+public  class Arrays79QScieLabExp {
+
+	public static void main(String[] args) 
+	{
+		int input1=5;
+		int input2=3;
+		int input3=3;
+		int output=input1+(input1+input2)+(input1+input2+input3);
+		
+		System.out.println(output);
+		
+	}
+
+}
