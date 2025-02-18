2e2c445375a706b0001188fcafb57950cbf96a1a
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2025-02-18 22:14:39 +0530

    Add files via upload

---
+++ DSA/FindConsonentLengthAndFactorial.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class FindConsonentLengthAndFactorial {
+
+	// Incomplete
+	public static void main(String[] args) 
+	{
+		String [] arr= {"hello","java","world","table"};
+		
+		for (String string : arr) {
+			//string=removeVowel(string);
+		}
+	}
+	
+
+}
