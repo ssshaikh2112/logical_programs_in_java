30b1d5366ad60a6ab8cad6cb828615dbfcf6480c
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2025-10-16 22:53:11 +0530

    Update StudentDriver.java

--- Interface/StudentDriver.java
+++ Interface/StudentDriver.java
@@ -11,8 +11,6 @@ class Student
 	Student(int id , String name , String edu ,int yop)
 	{
 		this.id=id;
-
-		
 		this.name=name;
 		this.edu=edu;
 		this.yop=yop;
@@ -41,6 +39,7 @@ class StudentDriver
 	}
 
 }
+
 
 
 
