5e41a8765be6331b335b565e7755dd866570c4ab
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2026-08-24 23:04:44 +0530

    Update SalaryIncrement.java

--- SalaryIncrement.java
+++ SalaryIncrement.java
@@ -3,6 +3,7 @@ class SalaryIncrement
 {
 	public static void main(String[] args) 
 	{
+		
 		Scanner scan=new Scanner(System.in);
 		System.out.println("Enter Your Salary : ");
 		double salary=scan.nextDouble();
